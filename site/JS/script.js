fd29b0451363e8e2cf31186686b7e4339f22462a
nitin <nitinalexvaidyan@gmail.com>
2022-09-17 23:17:36 +0530

    day end commit

---
+++ site/JS/script.js
@@ -0,0 +1,39 @@
+// var x2 ="Hello world!222!!";
+// console.log(x2);
+
+// function func1 () {
+// 	console.log("function is called");
+// } 
+
+// func1();
+
+// var n1;
+// console.log(n1);
+// if(x2=="hhss"){
+// 	console.log("true");
+// }
+// else {
+// 	console.log("false strat")
+// }
+
+// var obj1 = new Object();
+// obj1.name ="nitin";
+// obj1.age=28;
+// obj1.prof="engineer";
+// obj1.ceo= new Object();
+// obj1.ceo.firstname="Marjk" 
+// console.log(obj1)
+// console.log(obj1.name);
+// console.log(obj1["age"])
+// obj1["$stock of company"]=110;
+// console.log(obj1["$stock of company"])
+
+var home = {
+	place:"kallada",
+	type:"medium",
+	people:{
+		nitin:"son",
+		neetu:"daughter"
+	}
+};
+console.log(home);
